018c32f6bb5d74f983aa2b36626118dfd0fb18a9
marv's Mac <marvelmd@gmail.com>
2021-10-24 02:46:30 +0300

    bug fix

--- src/main/java/com/example/spring_course/Test3.java
+++ src/main/java/com/example/spring_course/Test3.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = context.getBean("myLovelyPey  ", Pet.class);
+        Pet pet = context.getBean("myLovelyPey", Pet.class);
         Person person = new Person(pet);
         person.callYourPet();
         context.close();
