74f6388eb731b5aff8398a72459389d5fcb2a239
marv's Mac <marvelmd@gmail.com>
2021-10-24 03:04:46 +0300

    inserted some informative messages in constructors

--- src/main/java/com/example/spring_course/Cat.java
+++ src/main/java/com/example/spring_course/Cat.java
@@ -1,6 +1,10 @@
 package com.example.spring_course;
 
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("cat bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Mew-miu!");
--- src/main/java/com/example/spring_course/Dog.java
+++ src/main/java/com/example/spring_course/Dog.java
@@ -1,6 +1,10 @@
 package com.example.spring_course;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("dog bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("bow-wow!");
