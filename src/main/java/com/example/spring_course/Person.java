9c56f2c29dda83705ebbd0ae9c3927133ee54f07
marv's Mac <marvelmd@gmail.com>
2021-10-29 16:54:52 +0300

    hibernate inserted Employee class created

--- src/main/java/com/example/spring_course/Person.java
+++ src/main/java/com/example/spring_course/Person.java
@@ -48,4 +48,13 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "pet=" + pet +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
